e90376004b63d932ebe25686ea4cea4691203810
Jayandra Hamal <salonhamal278@gmail.com>
2026-03-09 16:24:29 +0545

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListing, deleteListing, getListing, updateListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, getListing, updateListing, getListings } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.get('/listings', getListings);
 router.put('/update/:id', verifyToken, updateListing, getListing);
 router.get('/get/:id', getListing);
 
